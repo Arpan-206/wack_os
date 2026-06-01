d5b8577efd1a66cb1a9e166cb42451545e341845
Arpan Pandey <arpanpandey.aps@gmail.com>
2026-06-01 10:53:19 +0100

    Testing done?

---
+++ tests/basic_boot.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(wack_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use wack_os::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
+}
